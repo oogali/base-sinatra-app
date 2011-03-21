62af00083c2b7d13fa841a32ced2cb68b0fbc567
Omachonu Ogali <oogali@roadrunner.lab.idlepattern.com>
2011-03-21 04:07:16 -0400

    added placeholder text, tempo js, jquery

--- lib/appname/appname.rb
+++ lib/appname/appname.rb
@@ -3,5 +3,13 @@ require 'haml'
 
 module AppName
   class Application < Sinatra::Base
+    get '/' do
+      haml :index
+    end
+
+    get %r{/css/(default|reset)\.css} do
+      content_type 'text/css', :charset => 'utf-8'
+      sass :"#{params[:captures].first}"
+    end
   end
 end
