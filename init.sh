a57ea638dadb03dadfff27aefd2fa7c45a0ff4c7
Omachonu Ogali <oogali@gmail.com>
2011-09-23 23:06:40 -0400

    added class init script

---
+++ init.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+if [ $# -ne 1 ]; then
+  echo "$0 <new class name>"
+  exit 1
+fi
+
+if [ ! -f "Gemfile" ] || [ -f "init.sh" ]; then
+  echo "$0: this is not our base sinatra app"
+  exit 1
+fi
+
+newclass=${1}
+newpath=`echo ${1} | tr '[A-Z]' '[a-z]'`
+
+search=0
+while [ "${search}" == 0 ]; do
+  for file in `find . -name 'appname*' | sort -r` ; do
+    newfile=`echo ${file} | sed "s/appname/${newpath}/"`
+    mv ${file} ${newfile}
+  done
+
+  find . -name 'appname*' | grep appname
+  search=$?
+done
+
+for file in `find . -type f ! -path './.git/*' -exec grep -l appname {} \;` ; do
+  sed -i .bak -e "s/appname/${newpath}/g; s/AppName/${newclass}/g" ${file}
+  rm -f ${file}.bak
+done
+
+rm -f ${0}
