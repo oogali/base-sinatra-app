ce41e7e90825e74165a39ff842e35c7378b02c26
Omachonu Ogali <oogali@gmail.com>
2011-10-06 23:23:16 -0400

    refactor of migration out of dbexample

--- lib/appname/dbexample.rb
+++ lib/appname/dbexample.rb
@@ -3,18 +3,6 @@ require 'active_record'
 
 module AppName
   module DBModel
-    class CreateTable < ActiveRecord::Migration
-      def self.up
-        create_table :table do |t|
-          t.index :id
-        end
-      end
-
-      def self.down
-        drop_table :table
-      end
-    end
-
     class Table < ActiveRecord::Base
     end
   end
