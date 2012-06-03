2e9a6bb207415797497cdb505ed5e7b3b6a3f6b4
Omachonu Ogali <oogali@gmail.com>
2012-06-03 09:05:05 -0400

    add authentication support

--- lib/appname/app.rb
+++ lib/appname/app.rb
@@ -15,9 +15,47 @@ module AppName
       register Sinatra::Reloader
       enable :show_settings
     end
-    use Rack::Session::Pool
+
+    use Rack::Session::Pool, :path => '/', :secret => 'SET_YOUR_SECRET_SESSION_KEY_HERE', :key => 'SESSIONID', :sidbits => 128
     use Rack::Flash
 
+    helpers do
+      def authenticate!(username, password)
+        self.current_user = Db::User.authenticate username, password
+      end
+
+      def current_user
+        session[:user]
+      end
+
+      def current_user=(user)
+        session[:user] = user
+      end
+
+      def valid_session?
+        current_user and current_user.id.to_i > 0
+      end
+    end
+
+    before do
+      # unless valid_session? or (request.path_info.match /^\/(login|healthcheck|css\/|js\/)/)
+      #   session[:to] = request.path == '/login' ? '/' : request.path
+      #   return redirect '/login'
+      # end
+    end
+
+    aget '/login' do
+      body { haml :login }
+    end
+
+    post '/login' do
+      unless authenticate!(params[:email], params[:passwd])
+        flash[:error] = 'Sorry, you entered an invalid e-mail address and/or password.'
+        return redirect '/login'
+      end
+
+      redirect session[:to] ? session.delete(:to) : '/'
+    end
     aget '/' do
       body { haml :index }
     end
--- lib/appname/db.rb
+++ lib/appname/db.rb
@@ -1,4 +1,5 @@
 require 'active_record'
+require 'bcrypt'
 require 'will_paginate'
 
 module AppName
@@ -6,9 +7,18 @@ module AppName
     class Table < ActiveRecord::Base
     end
 
-    class Users < ActiveRecord::Base
-      def self.authenticate(username, password)
-        where(:username => username, :passwd => password).first
+    class User < ActiveRecord::Base
+      def password
+        @password ||= BCrypt::Password.new passwd
+      end
+
+      def password=(new_password)
+        self.passwd = BCrypt::Password.create new_password, :cost => 6
+      end
+
+      def self.authenticate(username, user_passwd)
+        user = self.find_by_username username
+        user.password == user_passwd ? user : nil
       end
     end
   end
