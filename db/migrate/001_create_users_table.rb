cddcf071939249ce9f27e4902472d891cf08b099
Omachonu Ogali <oogali@gmail.com>
2012-06-03 09:07:56 -0400

    create users table migration

---
+++ db/migrate/001_create_users_table.rb
@@ -0,0 +1,18 @@
+class CreateUsersTable < ActiveRecord::Migration
+  def self.up
+    create_table :users do |t|
+      t.primary_key :id
+      t.string :username, { :unique => true, :null => false }
+      t.string :passwd, :null => false
+      t.string :name
+      t.timestamps
+    end
+
+    # create new user account for oogali, with a default password
+    user = AppName::Db::User.create :username => 'test', :passwd => BCrypt::Password.create('changeme!', :cost => 6)
+  end
+
+  def self.down
+    drop_table :users
+  end
+end
