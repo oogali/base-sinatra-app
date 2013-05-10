0ebba6d25b0ff6e8fc7176436320d559da792f72
Omachonu Ogali <oogali@idlepattern.com>
2013-05-10 09:38:36 -0400

    Update capistrano configuration

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,17 +5,19 @@ set :scm, :git
 set :branch, :master
 set :git_enable_submodules, 1
 
-set :user, 'deploy'
+set :user, "#{application}"
 set :use_sudo, false
 
 set :copy_strategy, :export
-set :deploy_via, :copy
-set :deploy_to, "/home/#{user}/#{application}"
+# deploy_via:
+# * :copy means SCP our files to the remote host from the local host
+# * :remote_cache means check it out from the repository (e.g. Github, Bitbucket)
+set :deploy_via, :remote_cache
+set :deploy_to, "/home/#{application}"
 set :keep_releases, 5
 set :normalize_asset_timestamps, false
 
 ## hipchat deploy announcements
-# require 'hipchat/capistrano'
 # set :hipchat_token, 'HIPCHAT_API_TOKEN'
 # set :hipchat_room_name, 'HIPCHAT_ROOM_NAME'
 # set :hipchat_announce, true
@@ -24,16 +26,15 @@ role :app, 'localhost'
 role :db, 'localhost', :primary => true
 
 require 'bundler/capistrano'
+require 'hipchat/capistrano'
 
 namespace :deploy do
   task :start do
-    run "cd #{deploy_to}/current && #{rake} start"
+    run "cd #{deploy_to}/current && RACK_ENV=production #{rake} start"
   end
 
   task :stop do
-    if File.exists? "#{deploy_to}/current"
-      run "cd #{deploy_to}/current && #{rake} stop"
-    end
+    run "test -d #{deploy_to}/current && cd #{deploy_to}/current && RACK_ENV=production #{rake} stop"
   end
 
   task :restart, :roles => :app, :except => { :no_release => true } do
@@ -43,17 +44,9 @@ namespace :deploy do
 end
 
 namespace :config do
-  task :setup_log_path do
+  task :setup_environment do
     run "ln -s #{shared_path}/log #{deploy_to}/current/run"
   end
-
-  task :setup_environment do
-    run "cd #{deploy_to}/current && ln -sf config_production.yml config.yml"
-  end
-end
-
-before 'deploy:create_symlink', :roles => [ :app ] do
-  deploy.stop
 end
 
 after 'deploy:create_symlink', :roles => [ :db ] do
@@ -61,6 +54,5 @@ after 'deploy:create_symlink', :roles => [ :db ] do
 end
 
 after 'deploy:create_symlink', :roles => [ :app ] do
-  config.setup_log_path
   config.setup_environment
 end
