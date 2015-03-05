c99d30e09007ccc88a375d72ee09cdf960c75822
Omachonu Ogali <oogali@idlepattern.com>
2015-03-05 18:32:54 -0500

    Remove explicit primary key definition

--- db/migrate/001_create_users_table.rb
+++ db/migrate/001_create_users_table.rb
@@ -1,7 +1,6 @@
 class CreateUsersTable < ActiveRecord::Migration
   def self.up
     create_table :users do |t|
-      t.primary_key :id
       t.string :username, { unique: true, null: false }
       t.string :passwd, null: false
       t.string :name
