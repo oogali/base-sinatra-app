7bd80be5a77ac972b7c69c79e4fcb1d0c597158a
Omachonu Ogali <oogali@idlepattern.com>
2016-02-06 09:43:19 -0500

    Added conditional logic to reflect maintenance based on whether magic file exists

--- lib/appname/app.rb
+++ lib/appname/app.rb
@@ -97,6 +97,10 @@ module AppName
     end
 
     get '/healthcheck' do
+      if  File.exists?(File.join(settings.root, '.maintenance')) or File.exists?('/maintenance')
+        halt 503, 'We seem to be in a maintenance period. Be back shortly.'
+      end
+
       "OK\n"
     end
 
