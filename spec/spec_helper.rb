fae542441e626353a65e8bb883fc3c412d0d3b38
Omachonu Ogali <oogali@idlepattern.com>
2016-02-06 09:57:16 -0500

    Enabled shoulda-matchers

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,6 +18,15 @@ $LOAD_PATH << File.join(File.dirname(__FILE__), '../')
 Dir[File.join(File.dirname(__FILE__), '../setup', "*.rb")].each { |file| require file unless file.include?('setup/raven.rb') }
 require File.join(File.dirname(__FILE__), '../lib', 'appname.rb')
 
+require 'shoulda/matchers'
+Shoulda::Matchers.configure do |c|
+  c.integrate do |with|
+    with.test_framework :rspec
+    with.library :active_record
+    with.library :active_model
+  end
+end
+
 ENV['RACK_ENV'] = 'test'
 set :environment, :test
 set :run, :false
